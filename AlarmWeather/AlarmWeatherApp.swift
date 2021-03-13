de4263236cd7b8cb9856c140de43b83fa36e176c
badredaha <badre.daha@gmail.com>
2021-03-13 12:52:28 +0100

    First commit

---
+++ AlarmWeather/AlarmWeatherApp.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmWeatherApp.swift
+//  AlarmWeather
+//
+//  Created by Me on 10/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AlarmWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            ContentView()
+        }
+    }
+}
---
+++ AlarmWeather/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AlarmWeather
+//
+//  Created by Me on 10/01/2021.
+//
+
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
